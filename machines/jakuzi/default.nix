d96c6806501c10099a1948744789c439f7b889e9
jkz <j.k.zwaan@gmail.com>
2024-03-22 22:12:08 +0000

    Before

--- machines/jakuzi/default.nix
+++ machines/jakuzi/default.nix
@@ -1,36 +1,24 @@
-# Edit this configuration file to define what should be installed on
-# your system. Help is available in the configuration.nix(5) man page, on
-# https://search.nixos.org/options and in the NixOS manual (`nixos-help`).
-
-# NixOS-WSL specific options are documented on the NixOS-WSL repository:
-# https://github.com/nix-community/NixOS-WSL
-
-{ self, config, lib, pkgs, nixos-wsl, ... }:
+{ pkgs, ... }:
 {
   networking.hostName = "jakuzi";
 
   imports = [
-    ../wsl.nix
+    ../common.nix
     ../../home/vscode.nix
   ];
 
-  environment.variables = {
-    EDITOR = "vim";
+  # TODO currently struggling to avoid infinite recursion here
+  # imports = [
+  #   # <nixos-wsl/modules>
+  #   # flake-inputs.nixos-wsl.nixosModules.wsl
+  # ];
+
+  wsl = {
+    enable = true;
+    defaultUser = "jkz";
   };
 
-  environment.systemPackages = with pkgs; [
-    comma
-    nix-index
-    wget
-  ];
-
-  nix.settings.experimental-features = [ "nix-command" "flakes" ];
-
-  # This value determines the NixOS release from which the default
-  # settings for stateful data, like file locations and database versions
-  # on your system were taken. It's perfectly fine and recommended to leave
-  # this value at the release version of the first install of this system.
-  # Before changing this value read the documentation for this option
-  # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
-  system.stateVersion = "23.11"; # Did you read the comment?
+  environment.variables = {
+    EDITOR = "code";
+  };
 }
