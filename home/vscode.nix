45c8dff636484bedf4bae9868f514231a305eaa2
jkz <j.k.zwaan@gmail.com>
2024-03-26 01:01:37 +0000

    tidy

--- home/vscode.nix
+++ home/vscode.nix
@@ -1,4 +1,4 @@
-{ flake-inputs, ...}: {
+{ flake-inputs, nixpkgs, ...}: {
     imports = [
       flake-inputs.nixos-vscode-server.homeModules.default
     ];
@@ -7,6 +7,10 @@
     # Following instructions from https://nixos.wiki/wiki/Visual_Studio_Code
     services.vscode-server.enable = true; 
 
-    # This is a slightly hacky fix to make a file available that vscode needs to connect remotely
-    home.file.".vscode-server/server-env-setup".source = "${flake-inputs.vscode-remote-wsl}/server-env-setup";
+    home.file = {
+      # This is a slightly hacky fix to make a file available that vscode needs to connect remotely
+      ".vscode-server/server-env-setup".source = "${flake-inputs.vscode-remote-wsl}/server-env-setup";
+
+      ".vscode-server/data/Machine/settings.json".source = ../dotfiles/vscode/settings.json;
+    };
 }
